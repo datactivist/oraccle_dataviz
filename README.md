21b7356cbe9cfaf3232431f11b1c2af259a79906
DianeThy <diane.thierry.3asd@gmail.com>
2024-04-17 17:22:59 +0200

    début dataviz

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # oraccle_dataviz
+
 Data-visualisations des données des parcours d'études produites par le projet Oraccle
--- scripts/oraccle_2.R
+++ scripts/oraccle_2.R
@@ -1,18 +1,14 @@
+## Données du projet Oraccle - Université Sorbonne Paris Nord
+
+# Librairies
 library(tidyverse)
 library(jsonlite)
 
-cohortes_bacs <- read_delim("https://data.unif.app/data/cohortes_bacs.csv", ";")
-# effectifs par type de bacs
-cohortes <- read_delim("https://data.unif.app/data/cohortes.csv", ";")
-
+# Données
+    # Oraccle
+    # Référentiels
 etablissements <- read_delim("https://data.unif.app/data/etablissements.csv", ";")
-ips <- read_delim("https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-ips_colleges/exports/csv?lang=fr&qv1=(0921779J)&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B", ";")
 #data_ips : https://data.education.gouv.fr/explore/dataset/fr-en-ips_colleges/table/?disjunctive.rentree_scolaire&disjunctive.academie&disjunctive.code_du_departement&disjunctive.departement&disjunctive.uai&disjunctive.code_insee_de_la_commune&disjunctive.nom_de_la_commune&disjunctive.secteur&q=0921779J
-# public / privé, IPS, ecart-type
-
-cohortes_sexes <- read_delim("https://data.unif.app/data/cohortes_sexes.csv", ";")
-cohortes_uai <- read_delim("https://data.unif.app/data/cohortes_uai.csv", ";")
-# grande base par cohorte : nb établissements, sexes, bacs, options, % privé / public
 
 geoloc <- read_delim("https://data.unif.app/data/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre.csv", ";")
 # carto des établissements
@@ -34,3 +30,8 @@ cohorte_spe <- read_delim("./data/France/cohorte_spe.csv", ",")
 cohortes <- read_delim("./data/France/cohortes.csv", ",")
 formations <- read_delim("./data/France/formations.csv", ",")
 
+
+#----
+
+# Exploration 
+test <- table(cohorte_spe$bac_spe1) |> as.data.frame() |> arrange(desc(Freq))
